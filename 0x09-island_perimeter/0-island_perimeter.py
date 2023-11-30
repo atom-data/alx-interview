1c60648fb49e5380fd19343ca8cf370b7f19a92c
atom-data <alatatomondi@gmail.com>
2023-11-30 11:42:38 +0000

    with assistance

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""
-Define fuunction island_perimeter
-"""
+""" island_perimeter function """
 
 
 def island_perimeter(grid):
-    if not grid or not grid[0]:
-        return 0
+    """  returns the perimeter of the island described in grid """
 
-    rows = len(grid)
-    cols = len(grid[0])
+    def edges(matrix):
+        """ detect number of edges along horizontal direction """
+        count = 0
+        for row in matrix:
+            row = [0] + row + [0]
+            for i in range(1, len(row)):
+                count += row[i] != row[i-1]
+        return count
 
-    perimeter = 0
+    tgrid = [[] for _ in range(len(grid[0]))]
+    for row in grid:
+        for i, item in enumerate(row):
+            tgrid[i].append(item)
 
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += 4  # Add 4 for each land cell
-
-                # Check adjacent cells and deduct 1 for each adjacent land cell
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 1
-                if row < rows - 1 and grid[row + 1][col] == 1:
-                    perimeter -= 1
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 1
-                if col < cols - 1 and grid[row][col + 1] == 1:
-                    perimeter -= 1
-
-    return perimeter
+    return edges(grid) + edges(tgrid)
