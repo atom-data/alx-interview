4ab00cc35ea1282a09f6616baeb8e29645ae700a
atom-data <alatatomondi@gmail.com>
2023-11-30 11:33:44 +0000

    another attempt

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,15 +6,21 @@ Define function island_perimeter
 
 def island_perimeter(grid):
     perimeter = 0
+    for k in range(len(grid)):
+        grid[k].insert(0, 0)
+        grid[k].append(0)
+    new_row = [0 for k in range(len(grid[0]))]
+    grid.insert(0, new_row)
+    grid.append(new_row)
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                if grid[i - 1][j] != 1:
+                if grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i + 1][j] != 1:
+                if grid[i + 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] != 1:
+                if grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j + 1] != 1:
+                if grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
