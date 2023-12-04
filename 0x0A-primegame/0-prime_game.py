f7c05dbee89d3be15f4728ff99951dbd661e0252
atom-data <alatatomondi@gmail.com>
2023-12-04 16:09:02 +0000

    prime games

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Define isWinner function
+"""
+
+
+def isWinner(x, nums):
+    primes = []
+    winner_lst = []
+    max_elem = max(nums)
+    numbers = [i for i in range(1, max_elem + 1)]
+
+    for i in range(1, len(numbers)):
+        for j in numbers[i + 1:].copy():
+            if j % numbers[i] == 0:
+                numbers.remove(j)
+
+    for k in nums.copy():
+        for l in numbers.copy():
+            if l <= k:
+                primes.append(l)
+        psize = len(primes)
+        if psize % 2 == 0:
+            winner_lst.append('Maria')
+        else:
+            winner_lst.append('Ben')
+        primes.clear()
+
+    bcount = winner_lst.count('Ben')
+    wsize = len(winner_lst)
+    if bcount > (wsize - bcount):
+        most_freq = 'Ben'
+        count_diff = bcount - (wsize - bcount)
+    else:
+        most_freq = 'Maria'
+        count_diff = (wsize - bcount) - bcount
+
+    if count_diff >= (x - wsize):
+        return most_freq
+    else:
+        return None
